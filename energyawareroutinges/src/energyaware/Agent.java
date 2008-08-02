90b74386dd56b5143b286b2d8402078e27ce9f60
Alex Maskovyak <alexander.maskovyak@gmail.com>
2008-08-02 21:10:58 +0000

    Implementation of energy storage methods.

--- energyawareroutinges/src/energyaware/Agent.java
+++ energyawareroutinges/src/energyaware/Agent.java
@@ -22,16 +22,11 @@ public class Agent{
 	private WorkingMemoryMarker marker;	// JESS usage
 	private Node node;					// A reference to our node
 //  private Database database;
-	private PathTable pathTable;
-	private Map<Integer,Integer> batteryMetrics;
 
-	public int getBatteryMetrics( int pNodeID ){
-		return batteryMetrics.get( pNodeID );
-	}
+	private PathTable pathTable;
+	private Map<Integer, Integer> batteryMetrics;
+	private Map<NodePair, Integer> transmissionCosts;
 	
-	public void updateBatteryMetric( int pNodeID, int pMetricValue ){
-		batteryMetrics.put( pNodeID, pMetricValue );
-	}
 	
 	/**
 	 * Default agent constructor.
@@ -65,11 +60,23 @@ public class Agent{
 	}
 
 	
-	/// agent has to receive messages from node
-	/// agent has to send message to node
+	/**
+	 * Allow our Java Agent & JESS object to send data via the Node
+	 * 
+	 * @param pNode The node "we" reside in.
+	 */
+	public void setNode( Node pNode ) throws JessException {
+		
+		node = pNode;
+		engine.add( this );
+		engine.run();
+	}
 	
-	/// agent has to receive frames from node
-	/// agent has to send frames from the node
+	
+	public Rete getEngine() {
+		return engine;
+	}
+	
 	
 	/**
 	 * Obtain a segment from up in the transport layer.  This segment is to be
@@ -124,19 +131,7 @@ public class Agent{
 	}
 	
 	
-	/**
-	 * Allow our Java Agent & JESS object to send data via the Node
-	 * 
-	 * @param pNode The node "we" reside in.
-	 */
-	public void setNode( Node pNode ) throws JessException {
-		
-		node = pNode;
-		engine.add( this );
-		engine.run();
-	}
-	
-	
+
 	/**
 	 * Obtain a datagram from down in the datalink layer.  This datagram is 
 	 * passed to JESS.
@@ -168,7 +163,6 @@ public class Agent{
 	
 	/**
 	 * Yep you guessed it.  Get the ID
-	 * 
 	 * @return Take a guess
 	 */
 	public int getID() {
@@ -186,62 +180,261 @@ public class Agent{
 		
 	}
 	
+	////
+	////
+	//// Battery related information storage and maintenance.
+	////
+	////
+	
+	
+	/**
+	 * Obtain the node's battery metric.
+	 * @return
+	 */
 	public int getBatteryMetric() {
 		
 		return BatteryMetric.calculateBatteryMetric(
 				node.getBattery() );
 	}
-	
-	public Rete getEngine() {
-		return engine;
+
+	/**
+	 * Obtain the battery metric for a given node.
+	 * @param pNodeID Node for which to obtain a battery metric.
+	 * @return Battery metric for the specified node.
+	 */
+	public int getBatteryMetrics( int pNodeID ){
+		return batteryMetrics.get( pNodeID );
 	}
 	
-	private PathTable pathtable;
+	/**
+	 * Update the battery metric for a given node id. 
+	 * @param pNodeID Node ID for which to store the specified battery metric.
+	 * @param pMetricValue Battery metric value to associated with the Node ID.
+	 */
+	public void updateBatteryMetric( int pNodeID, int pMetricValue ){
+		batteryMetrics.put( pNodeID, pMetricValue );
+	}
 	
-	public void updatePathTable( ArrayList<Integer> pPath ) {
+	/**
+	 * Updates our Map of nodes to battery metrics.  
+	 * @param pNodeIDs
+	 * @param pMetricValues
+	 */
+	public void updateBatteryMetrics( 
+			ArrayList<Integer> pNodeIDs,
+			ArrayList<Integer> pMetricValues ) {
 		
+		int metricSize = pMetricValues.size();
+		
+		// ensure that we don't have more metric values than nodes
+		if ( metricSize > pNodeIDs.size() ) {
+			return;
+		}
+		
+		// pull each pair of metric values and pair
+		for (int i = 0; i < metricSize; ++i) {
+			int id = pNodeIDs.get( i );
+			int metric = pMetricValues.get( i );
+			updateBatteryMetric( id, metric );
+		}
 	}
 	
-	public void addPath( ArrayList<Integer> pPath ){
+	
+	
+	////
+	////
+	//// Path related storage and maintenance.
+	////
+	////
+	
+	/**
+	 * Add the provided path to the list of paths.
+	 * @param pPath Path to add to the list of paths.
+	 */
+	public void updatePathTable( ArrayList<Integer> pPath ){
 		pathTable.addPath(pPath);
 	}
 	
+	/**
+	 * Determine whether we currently have the path.
+	 * @param pPath
+	 * @return
+	 */
+	public boolean hasPath( ArrayList<Integer> pPath ) {
+		return pathTable.hasPath( pPath );
+	}
+	
+	/**
+	 * Holds an association: destinations have a pathset of paths which can be
+	 * used to route on the network.
+	 * @author Steve Baylor, Jeff Corcoran & Alex Maskovyak
+	 *
+	 */
 	private class PathTable {
 		
 		private Map<Integer,PathSet> pathSetMap;
 		
+		/**
+		 * Default constructor.
+		 */
 		public PathTable() {
 			pathSetMap = new HashMap<Integer,PathSet>();
 		}
 		
-		public ArrayList<Integer> getPath() {
-			return null;
+		/**
+		 * Obtains a set of paths that end at a given destination.
+		 * @return PathSet of paths to a given destination.
+		 */
+		public PathSet getPathSet( int pDestination ) {
+			return pathSetMap.get( pDestination );
 		}
 		
+		/**
+		 * Determine whether the given path is stored in our pathtable.
+		 * @param pPath Path whose occupancy is in question.
+		 * @return True if we have the path stored, false otherwise.
+		 */
+		public boolean hasPath( ArrayList<Integer> pPath ) {
+			int dest = pPath.get( pPath.size()-1 );
+			
+			return pathSetMap.get( dest ).hasPath(pPath);
+		}
+		
+		/**
+		 * Add an additional path to a set of paths associated with a given
+		 * destination node id.
+		 * @param pPath Path to a given destination, this path will be added to
+		 * 			the pathset for that destination.
+		 */
 		public void addPath( ArrayList<Integer> pPath ) {
-			int dest = pPath.get(pPath.size()-1);
-			PathSet pathSet = pathSetMap.get(dest);
+			int dest = pPath.get( pPath.size()-1 );
+			PathSet pathSet = pathSetMap.get( dest );
+			
 			if ( pathSet != null ){	//destination already exists in map
 				pathSet.addPath( pPath );
 			} 
 			else {				// destination does not exist yest
 				PathSet tempSet = new PathSet();
-				tempSet.addPath(pPath);
+				tempSet.addPath( pPath );
 				pathSetMap.put( dest, tempSet );
 			}
 		} 
 		
-		private class PathSet {
+		/**
+		 * Removes path to a destination device.
+		 * @param pPath Path to remove.
+		 */
+		public void removePath( ArrayList<Integer> pPath ) {
+			int dest = pPath.get( pPath.size()-1 );
+			PathSet pathSet = pathSetMap.get( dest );
+			
+			if ( pathSet != null ) {
+				pathSet.removePath( pPath );
+			}
+		}
+		
+		/**
+		 * Simple wrapper for an array-list of a path.
+		 * @author Steve Baylor, Jeff Corcoran & Alex Maskovyak
+		 *
+		 */
+		public class PathSet {
 			
 			private Set <ArrayList<Integer>> paths;
 			
+			/**
+			 * Default constructor.
+			 */
 			public PathSet() {
 				paths = new HashSet<ArrayList<Integer>>();
 			}
 			
-			public void addPath(ArrayList<Integer> pPath){
-				paths.add(pPath);
+			/**
+			 * Adds the arraylist as a path to our pathset.
+			 * @param pPath Path to add to this set.
+			 */
+			public void addPath( ArrayList<Integer> pPath ){
+				paths.add( pPath );
 			}
+			
+			/**
+			 * 
+			 * @param pPath
+			 * @return
+			 */
+			public boolean hasPath( ArrayList<Integer> pPath ) {
+				return paths.contains(pPath);
+			}
+			
+			/**
+			 * Removes the specified arraylist path from our pathset.
+			 * @param pPath Path to remove from this set.
+			 */
+			public void removePath( ArrayList<Integer> pPath ) {
+				paths.remove( pPath );
+			}
+		}
+	}
+	
+	
+	////
+	////
+	//// Transmission cost related storage and maintenance.
+	//// 
+	////
+	
+	/**
+	 * 
+	 */
+	public void updateTransmissionCost( NodePair pNodePair, int pCost ) {
+		transmissionCosts.put( pNodePair, pCost );
+	}
+	
+	/**
+	 * Update the transmission hop cost table.
+	 * @param pNodeIDs
+	 * @param pTransmissionCosts
+	 */
+	public void updateTransmissionCosts( 
+			ArrayList<Integer> pNodeIDs,
+			ArrayList<Integer> pTransmissionCosts) {
+		
+		// parameter sanity check
+		int transSize = pTransmissionCosts.size();
+		if ( transSize > pNodeIDs.size() - 1 ) {
+			return;
+		}
+		
+		// pull out node pairs (hops) and associate a cost with them
+		for (int i = 0; i < transSize; ++i) {
+			int cost = pTransmissionCosts.get( i );
+			int nodeA = pNodeIDs.get( i );
+			int nodeB = pNodeIDs.get( i + 1 );
+			NodePair nodePair = new NodePair( nodeA, nodeB );
+			
+			updateTransmissionCost( nodePair, cost );
+		}
+	}
+	
+	
+	/**
+	 * Designed to aid in hop transmission cost storage.
+	 * @author Steve Baylor, Jeff Corcoran & Alex Maskovyak
+	 *
+	 */
+	private class NodePair {
+		
+		private int nodeA;
+		private int nodeB;
+		
+		/**
+		 * Constructor.
+		 * @param pNodeA
+		 * @param pNodeB
+		 */
+		public NodePair( int pNodeA, int pNodeB ) {
+			nodeA = Math.min( pNodeA, pNodeB );
+			nodeA = Math.max( pNodeA, pNodeB );
 		}
 	}
 }
--- energyawareroutinges/src/energyaware/TestSuite.java
+++ energyawareroutinges/src/energyaware/TestSuite.java
@@ -3,6 +3,8 @@ package energyaware;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
 import jess.*;
 
 /**
@@ -22,7 +24,7 @@ public class TestSuite {
 		try {
 			
 		System.out.println( "TEST 1:\t"); TestRuleInit1();
-		System.out.println( "TEST 2:\t" ); TestUniversalRule1();
+		System.out.println( "TEST 2:\t" ); TestRuleUniversal1();
 //		System.out.println( "TEST 3:\t" + TestRule3() );
 //		System.out.println( "TEST 4:\t" + Test4() );
 //		System.out.println( "TEST 5:\t" + Test5() );
@@ -66,47 +68,54 @@ public class TestSuite {
 		}
 	}
 	
-	public void TestUniversalRule1() throws JessException {
+	public void TestRuleUniversal1() throws JessException {
+		int source = 1;
+		int middle = 2;
+		int destination = 3;
 		
-		Node node1 = Node.getInstance( 1 );
+		Node node1 = Node.getInstance( source );
 		
+		Agent agent = node1.getAgent();
+		Rete engine = agent.getEngine();
+		Datagram dg = 
+			new Datagram (
+				Datagram.UNINIT,
+				source,
+				destination);
+		
+		ArrayList<Integer> path = TestSuite.makeList( source, middle, destination );
+		dg.setPath( path );
+		agent.receiveDatagram( dg, 10 );
+		engine.add( dg );
+		
+		// make sure we have a datagram in there
+		if ( engine.containsObject( dg ) ) {
+			
+			System.out.println( "UR1: Datagram inserted properly." );
+		}
+		
+		// check our path
+		if ( agent.hasPath( path ) ) {
+			
+			System.out.println( "UR1: Path succesfully added to table." );
+		}
+		
+		// check our battery metric
+		
+		// check our transmission cost
 		
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	private static ArrayList<Integer> makeList( Integer ... integers ) {
+		ArrayList <Integer> a = new ArrayList<Integer>();
+		
+		for (Integer i : integers) {
+			a.add( i );
+		}
+		
+		return a;
+	}
 /*
 	public static void TestN() {
 		
@@ -134,13 +143,21 @@ public class TestSuite {
 //		node.receiveFrame(frame2);
 	}
 	
-	private static ArrayList<Integer> makeList() {
-		ArrayList <Integer> a = new ArrayList<Integer>();
-		a.add( 1 );
-		a.add( 789 );
-		return a;
+	
+	
+<<<<<<< .mine
+	public TestSuite() {
+		System.out.println( "TEST 1:\t" + Test1() );
+		System.out.println( "TEST 2:\t" + Test2() );
+		System.out.println( "TEST 3:\t" + Test3() );
+		System.out.println( "TEST 4:\t" + Test4() );
+		System.out.println( "TEST 5:\t" + Test5() );
+		System.out.println( "TEST 6:\t" + Test6() );
+		System.out.println( "TEST 7:\t" + Test7() );
 	}
 	
+=======
+>>>>>>> .r43
 	// Test an empty battery
 	private String Testb1() {
 	
